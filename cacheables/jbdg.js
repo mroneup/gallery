7dbf964bcbd5547bf2547edff26ab5d9c6a4cada
mroneup <42872133+mroneup@users.noreply.github.com>
2021-02-09 12:38:52 +0100

    first version of jdbg, together with the first file using it

---
+++ cacheables/jbdg.js
@@ -0,0 +1,29 @@
+function whatisthis(){
+  console.log("These are jacob's depression gadgets!")
+}
+function feelbetter(text){
+  document.getElementById("content").innerHTML = text;
+}
+function setcolor(color){
+  document.body.style.backgroundColor = color;
+  return "it is done.";
+}
+function setcookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getcookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
